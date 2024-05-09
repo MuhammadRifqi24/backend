e5a598830b2004343c4be8a428548e62248d3258
fikagithub <fikaadilah24@gmail.com>
2024-05-09 21:14:22 +0700

    belajar1

---
+++ routes/roles/apiOwner.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Owner;
+
+// * {BASE_URL}/api/v1/owner
+Route::get('/', function () {
+    return response()->json([
+        'status' => true,
+        'message' => 'Selamat Datang Stan'
+    ]);
+});
+
+Route::middleware(['auth:sanctum', 'checkVerifyEmail', 'checkRole:owner'])->group(function () {
+    Route::get('product/index', [Owner\ProductController::class, 'index']);
+    Route::post('product/insert', [Owner\ProductController::class, 'insert']);
+    Route::post('product/update', [Owner\ProductController::class, 'update']);
+    Route::delete('product/delete', [Owner\ProductController::class, 'destroy']);
+});
