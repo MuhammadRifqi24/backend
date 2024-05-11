4bb05495632febe7d23bdaf8cdf9dab93fcc8dbd
mirza purnandi <mirza.purnandi@gmail.com>
2024-05-11 23:46:27 +0700

    Add Handle Error Exception and add Folder in Public

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Http\Middleware as Mid;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -29,5 +34,85 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (Throwable $exception, Request $request) {
+            if ($exception instanceof InvalidArgumentException) {
+                $code = 500;
+                $message = "kesalahan InvalidArgumentException, kirim email gagal ...";
+                return response()->json([
+                    'status' => false,
+                    'message' => $message,
+                    'result' => [
+                        'file' => $exception->getFile(),
+                        'line' => $exception->getLine(),
+                        'code' => $code,
+                        'message' => $exception->getMessage()
+                    ]
+                ], $code);
+            } else if ($exception instanceof MethodNotAllowedHttpException) {
+                $code = $exception->getStatusCode();
+                $message = "Method URL tidak diizinkan";
+                return response()->json([
+                    'status' => false,
+                    'message' => $message,
+                    'result' => [
+                        'file' => $exception->getFile(),
+                        'line' => $exception->getLine(),
+                        'code' => $code,
+                        'message' => $exception->getMessage()
+                    ]
+                ], $code);
+            } else if ($exception instanceof AuthenticationException) {
+                $code = Response::HTTP_UNAUTHORIZED;
+                $message = "Sesi token anda sudah habis, silahkan login ulang";
+                return response()->json([
+                    'status' => false,
+                    'message' => $message,
+                    'result' => [
+                        'file' => $exception->getFile(),
+                        'line' => $exception->getLine(),
+                        'code' => $code,
+                        'message' => $exception->getMessage()
+                    ]
+                ], $code);
+            } else if ($exception instanceof TypeError) {
+                $code = Response::HTTP_INTERNAL_SERVER_ERROR;
+                $message = "Jangan asal bro";
+                return response()->json([
+                    'status' => false,
+                    'message' => $message,
+                    'result' => [
+                        'file' => $exception->getFile(),
+                        'line' => $exception->getLine(),
+                        'code' => $code,
+                        'message' => $exception->getMessage()
+                    ]
+                ], $code);
+            } else if ($exception instanceof ArgumentCountError) {
+                $code = Response::HTTP_REQUEST_TIMEOUT;
+                $message = "Argument tidak lengkap";
+                return response()->json([
+                    'status' => false,
+                    'message' => $message,
+                    'result' => [
+                        'file' => $exception->getFile(),
+                        'line' => $exception->getLine(),
+                        'code' => $code,
+                        'message' => $exception->getMessage()
+                    ]
+                ], $code);
+            } else if ($exception instanceof NotFoundHttpException) {
+                $code = $exception->getStatusCode();
+                $message = "Route API tidak tersedia";
+                return response()->json([
+                    'status' => false,
+                    'message' => $message,
+                    'result' => [
+                        'file' => $exception->getFile(),
+                        'line' => $exception->getLine(),
+                        'code' => $code,
+                        'message' => $exception->getMessage()
+                    ]
+                ], $code);
+            }
+        });
     })->create();
