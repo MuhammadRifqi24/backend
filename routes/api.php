e1c494c887b20b3452007398499c19b8a577d5e4
sultansyah <muhammadsultansyah14@gmail.com>
2024-05-23 01:33:47 +0700

    fix: ubah kata registerCustomer jadi registerUser

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 });
 Route::post('auth/login', [API\AuthController::class, 'login']);
 Route::post('auth/register-owner', [API\RegisterController::class, 'registerOwner']);
-Route::post('auth/register-user', [API\RegisterController::class, 'registerCustomer']);
+Route::post('auth/register-user', [API\RegisterController::class, 'registerUser']);
 Route::get('auth/verify-email', [API\AuthController::class, 'getVerifyEmail']);
 Route::post('auth/verify-email', [API\AuthController::class, 'verifyEmail']);
 Route::post('auth/verify-email-owner', [API\AuthController::class, 'verifyEmailOwner']);
