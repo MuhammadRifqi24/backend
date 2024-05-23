4796b642ca304f1f0c6a35e38d6a3ccf1e5f3c66
sultansyah <muhammadsultansyah14@gmail.com>
2024-05-23 20:20:20 +0700

    feat: Tambah Route API Order User(Customer) getAll, getByUUid, dan Insert

--- routes/roles/apiUser.php
+++ routes/roles/apiUser.php
@@ -15,4 +15,8 @@ Route::middleware(['auth:sanctum', 'checkVerifyEmail', 'checkRole:user'])->group
     Route::get('table-info/cafe/{id}', [User\TableInfoController::class, 'index']);
     Route::post('table-info/book-table', [User\TableInfoController::class, 'bookTable']);
     Route::post('table-info/finish-table', [User\TableInfoController::class, 'finishTable']);
+
+    Route::get('order/index', [User\OrderController::class, 'index']);
+    Route::get('order/uuid/{uuid}', [User\OrderController::class, 'getByUUID']);
+    Route::post('order/insert', [User\OrderController::class, 'insert']);
 });
