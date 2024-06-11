c8f5e725af9d00d8ef7efb3bce7485dbe9f005f3
sultansyah <muhammadsultansyah14@gmail.com>
2024-06-11 14:59:57 +0700

    feat: tambah API Index dan update untuk stan dan tambah upload logo untuk stan

--- app/Services/Sultan/FileUploadService.php
+++ app/Services/Sultan/FileUploadService.php
@@ -59,24 +59,69 @@ class FileUploadService
         try {
             $manager = new ImageManager(new Driver());
             if ($update != null) {
-                $cekimage = public_path('images/cafe/' . $update);
+                $cekimage = public_path('images/stan/' . $update);
                 if (file_exists($cekimage)) unlink($cekimage);
-                $cekthumbnail = public_path('images/cafe/thumbnail/' . $update);
+                $cekthumbnail = public_path('images/stan/thumbnail/' . $update);
                 if (file_exists($cekthumbnail)) unlink($cekthumbnail);
             }
-            $image = $file->move(public_path() . '/images/cafe/temp/', $name);
+            $image = $file->move(public_path() . '/images/stan/temp/', $name);
             //resize image aspect ratio
             $original = $manager->read($image);
             if ($original->width() > 800) {
                 $original->scale(800, 800);
             }
-            $ori = $original->save(public_path('images/cafe/' . $name));
+            $ori = $original->save(public_path('images/stan/' . $name));
 
             $thumbnail = $manager->read($image);
             if ($thumbnail->width() > 250) {
                 $thumbnail->scale(250, 250);
             }
-            $thumb = $thumbnail->save(public_path('images/cafe/thumbnail/' . $name));
+            $thumb = $thumbnail->save(public_path('images/stan/thumbnail/' . $name));
+
+            if ($ori && $thumb) {
+                if (file_exists($image)) unlink($image);
+            }
+
+            $status = true;
+        } catch (\Exception $e) {
+            $status = false;
+            $result = [
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage()
+            ];
+        }
+
+        return [
+            'status' => $status,
+            'result' => $result
+        ];
+    }
+
+    public static function uploadStan($file, $name, $update = null)
+    {
+        $result = [];
+        try {
+            $manager = new ImageManager(new Driver());
+            if ($update != null) {
+                $cekimage = public_path('images/stan/' . $update);
+                if (file_exists($cekimage)) unlink($cekimage);
+                $cekthumbnail = public_path('images/stan/thumbnail/' . $update);
+                if (file_exists($cekthumbnail)) unlink($cekthumbnail);
+            }
+            $image = $file->move(public_path() . '/images/stan/temp/', $name);
+            //resize image aspect ratio
+            $original = $manager->read($image);
+            if ($original->width() > 800) {
+                $original->scale(800, 800);
+            }
+            $ori = $original->save(public_path('images/stan/' . $name));
+
+            $thumbnail = $manager->read($image);
+            if ($thumbnail->width() > 250) {
+                $thumbnail->scale(250, 250);
+            }
+            $thumb = $thumbnail->save(public_path('images/stan/thumbnail/' . $name));
 
             if ($ori && $thumb) {
                 if (file_exists($image)) unlink($image);
